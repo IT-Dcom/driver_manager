10bafb436b169df568b6f149844d22fd7e477762
Alexandre Lairan <lairana@free.fr>
2016-05-30 13:57:33 +0200

    Add user firstname, lastname

--- db/migrate/20160522154641_devise_create_users.rb
+++ db/migrate/20160522154641_devise_create_users.rb
@@ -30,6 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :firstname, null: false
+      t.string :lastname, null: false
 
       t.timestamps null: false
     end

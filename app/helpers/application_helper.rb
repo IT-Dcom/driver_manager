8575375552575a061b81e08ed15e847e0ac5786c
Alexandre Lairan <lairana@free.fr>
2016-06-02 14:01:49 +0200

    Mise en place de la page profil et traduction d elements Devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,33 @@
 #
 module ApplicationHelper
+  # value = {
+  #           input: [{ name: firstname, value: Alexandre}],
+  #           span: { class1: %w(input-group-addon),
+  #                   class2: %w(glyphicon glyphicon-user) }
+  #         }
+
+  # def input_field(value)
+  #   res = "<div class='col-sm-6'>\n"
+  #   value[:input].each do |input|
+  #     res << "\t<div class='input-group'>\n"
+  #     res << "\t\t<input name='#{input[:name]}' class='form-control'\n"
+  #     res << "\t\t\tvalue='#{{input[:value]}}'>\n"
+  #
+  #   end
+  #   res << "\t</div>\n"
+  #   res << "</div>\n"
+  #
+  #
+  #     <div class='input-group'>
+  #       <input name='firstname' class='form-control'
+  #         value='<%= current_user.firstname %>' disabled='true'>
+  #       </input>
+  #       <input name='lastname' class='form-control'
+  #              value='<%= current_user.lastname %>' disabled='true'>
+  #       </input>
+  #       <span class='input-group-addon'>
+  #         <span class='glyphicon glyphicon-user'></span>
+  #       </span>
+  #     </div>
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160529125823) do
-  create_table "clients", force: :cascade do |t|
-    t.datetime "name"
-    t.datetime "email"
-    t.datetime "address"
-    t.datetime "zip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "drivers", force: :cascade do |t|
     t.datetime "created_at",             null: false
@@ -84,4 +76,5 @@ ActiveRecord::Schema.define(version: 20160529125823) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end

d876a9b4b5a47b03ab5811d5b889a0af7c8a7544
Alexandre Lairan <lairana@free.fr>
2016-06-08 11:21:57 +0200

    Edit rubocop rules to be more flexible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,29 @@
 #
 module ApplicationHelper
   # value = {
-  #           input: [{ name: firstname, value: Alexandre, type: :text_field_tag, disabled: true}],
+  #           input: [{ name: firstname, value: Alexandre,
+  #                     type: :text_field_tag, disabled: true}],
   #           span: { class1: %w(input-group-addon),
   #                   class2: %w(glyphicon glyphicon-user) }
   #         }
   TYPE = { text: :text_field_tag, password: :password_field_tag }.freeze
 
   def input_field(value)
-    content_tag :div, class: "col-sm-6" do
-      content_tag :div, class: "input-group" do
+    content_tag :div, class: 'col-sm-6' do
+      content_tag :div, class: 'input-group' do
         value[:input].map do |input|
-          concat text_field_tag input[:name], input[:value], name: input[:name], class: 'form-control', disabled: input[:disabled]
+          concat text_field_tag input[:name],
+                                input[:value],
+                                name: input[:name],
+                                class: 'form-control',
+                                disabled: input[:disabled]
         end
 
-        concat (content_tag :span, nil, class: value[:span][:class1] do
+        content = content_tag :span, nil, class: value[:span][:class1] do
           content_tag :span, nil, class: value[:span][:class2]
-        end)
+        end
+
+        concat content
       end
     end
   end
